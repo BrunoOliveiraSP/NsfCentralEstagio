df342ee7fe7a2ee22cd4771ac6a02ea62dbab7ee
Bruno de Oliveira <brunooliveira.me@outlook.com>
2020-03-06 17:46:30 -0300

    teste2

--- Models/centralestagio_dbContext.cs
+++ Models/centralestagio_dbContext.cs
@@ -34,7 +34,6 @@ namespace nsfcentralestagio.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseMySql("server=localhost;user id=root;password=1234;database=centralestagio_db");
             }
         }
